5c0fc451345fcd5b9a1cfa4f25919b676e6ec689
Mohammed Bilal Nadeem <mdbilalnad117@gmail.com>
2026-06-16 10:56:56 +0530

    Add files via upload

---
+++ escape.py
@@ -0,0 +1,10 @@
+#THIS IS ABOUT ESCAPING CHARACTERS IN PYTHON
+new_string = "This is a string with a newline character.\nThis is the second line."
+print(new_string)
+tabbed_string = "This is a string with a tab character.\tThis is after the tab."
+print(tabbed_string)
+Double_quoted_string = "This is a string with a double quote character: \"This is in double quotes.\""
+print(Double_quoted_string)
+
+name = "Bilal"
+print(f"GOOD AFTERNOON {name}")
